4de32f1389a7c9ef5a8065d8a5ea0f97fa2fd518
Jason Nutt <jnutt367@gmail.com>
2021-04-15 19:12:40 -0400

    commit for final meet the freds version for the day

--- gsap.js
+++ gsap.js
@@ -2,6 +2,14 @@ gsap.set("#demo", { scale: "0.6" });
 
 gsap.timeline()
     .from("#demo", { duration: 1, opacity: 0 })
-    .from("#title", { opacity: "0", scale: "0", ease: "back" })
+    .from("#title", { duration: 2, opacity: "0", scale: "0", ease: "back" })
     .from("#freds img", { y: 160, stagger: 0.1, duration: 0.8, ease: "back" })
     .from("#time", { xPercent: 100 })
+    .to("#purple", { y: 200 })
+    .to("#red", {
+        x: -200,
+        y: -50,
+        rotate: "360",
+        scale: "2"
+    })
+    .to("#fred", { duration: 1.5, scale: "1.2", ease: "bounce" })
