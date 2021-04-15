2beddb155d1f58732627a522e898acde735345fc
Jason Nutt <jnutt367@gmail.com>
2021-04-15 18:40:20 -0400

    commit first timeline

--- gsap.js
+++ gsap.js
@@ -1,1 +1,7 @@
-gsap.to('h1', { color: "green" })
+gsap.set("#demo", { scale: "0.6" });
+
+gsap.timeline()
+    .from("#demo", { duration: 1, opacity: 0 })
+    .from("#title", { opacity: "0", scale: "0", ease: "back" })
+    .from("#freds img", { y: 160, stagger: 0.1, duration: 0.8, ease: "back" })
+    .from("#time", { xPercent: 100 })
