6cbeea8382233ab7d9557d11892a81a804ef744f
Jason Nutt <jnutt367@gmail.com>
2021-04-16 16:48:36 -0400

    commit with version having all freds animate ate the end

--- controls.js
+++ controls.js
@@ -1,9 +1,12 @@
 var animation = gsap.timeline()
     .from("#demo", { duration: 1, opacity: 1 })
-    .from("#title", { opacity: 0, duration: 3, scale: 0, ease: "back" })
-    .from("#freds img", { y: 160, stagger: 0.5, duration: 0.8, ease: "back" }, "+=0.5")
+    .from("#title", { opacity: 0, duration: 2, scale: 0, ease: "back" })
+    .from("#freds img", { y: 160, stagger: 0.2, duration: 0.8, ease: "back" }, "+=0.5")
     .add("test")
-    .from("#time", { xPercent: 100, duration: 1, ease: "bounce" });
+    .from("#time", { xPercent: 100, duration: 1, ease: "elastic" })
+    .to("#red", { x: -200, y: -50, rotate: "360", scale: "2" })
+    .to("#fred", { duration: 2, x: 200, y: -40, rotate: "-360", scale: "1.5", ease: "elastic" })
+    .to("#orange", { duration: ".5", x: "-220", })
 
 
 
